1dbdd68b88a994790d970bfb6cde97fa062f4e76
Woncheol Kim <icijiy@gmail.com>
2023-04-04 14:11:06 -0700

    Used props for expenses

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,12 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-    // 2. 그래서 자바스크립트 코드를 제공한다.
-    const expenseDate = new Date(2023, 4, 3);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67;
-
-
-    // 1. 리액트에서 아래와 같이 html로 데이터를 고정으로 입력할 수 있으나 그렇게 하지 않는다.
-    // 3. JSX코드를 사용해 자바스크립트 코드를 실행하도록 한다.
+function ExpenseItem(props) {
     return (
     <div className="expense-item">
-        <div>{expenseDate.toISOString()}</div>
+        <div>{props.date.toISOString()}</div>
         <div className="expense-item__description">
-            <h2>{expenseTitle}</h2>
-            <div className="expense-item__price">${expenseAmount}</div>
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">${props.amount}</div>
         </div>
     </div>
     );

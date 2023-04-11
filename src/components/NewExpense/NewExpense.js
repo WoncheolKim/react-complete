e52aa008842179dfc332a179b4a0bd5d7b378e7a
Woncheol Kim <icijiy@gmail.com>
2023-04-11 14:15:57 -0700

    Added New input form on the title

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './NewExpense.css';
-
+import ExpenseForm from './ExpenseForm';
 
 const NewExpense = () => {
-    return <div className="new-expense">
-
-        
+    return (
+    <div className="new-expense">
+        <ExpenseForm />
     </div>
+    )
 };
 
 export default NewExpense;
